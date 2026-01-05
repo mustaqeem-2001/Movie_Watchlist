50a2dcf45e211089900775304bb70ae65d0ce651
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2026-01-05 18:40:35 +0000

    Displays movies onto screen, no styling done.

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 const searchBtn = document.getElementById("searchBtn");
+const filmWrapper = document.getElementById("filmWrapper");
 
-searchBtn.addEventListener("click", function(e) {
+searchBtn.addEventListener("click", async function(e) {
     e.preventDefault();
-    const searchInput = document.getElementById("searchInput").value;
-    // const replacedText = searchInput.replace(/ /g, "+");
-    // console.log(replacedText);
-    fetch(`http://www.omdbapi.com/?apikey=49133b6f&t=${searchInput}`)
-        .then(response => response.json())
-        .then(function(data) {
-            console.log(data);
-
-        });
+    const htmlOutput = await getData();
+    filmWrapper.innerHTML = htmlOutput;
 })
 
+async function getData() {
 
+    // Gets list of movies that matches the search input
+    const searchInput = document.getElementById("searchInput").value;
+    const searchMovies = await fetch(`http://www.omdbapi.com/?apikey=49133b6f&s=${searchInput}&type=movie`);
+    const searchData = await searchMovies.json();
+    
+    // Gets specific movie details that are missing from the previous fetch response.    
+    //  Same as searchMovies.Search array but, this time not just basic data, the whole details of each movie.
 
+    const searchMoviesDetailed = searchData.Search.map(movie => {
+        return fetch(`http://www.omdbapi.com/?apikey=49133b6f&i=${movie.imdbID}&type=movie`)
+            .then(response => response.json())
+    }) 
+    console.log(searchMoviesDetailed); // Displays Promises as an array
 
-let template = `
-    <div class="movie-container">
-        <img class="movie-poster" src="${movie.Poster}" alt=""/>
-        <div class="movie-details">
-            <h2 class="movie-title">${movie.Title}</h2>
-            <div class="movie-rating">${movie.imdbRating}</div>
-            <div class="movie-year">${movie.Year}</div>
-            <div class="add-movie-to-watchlist"> 
-                <i></i>
-                Watchlist
+    const movies = await Promise.all(searchMoviesDetailed) // Solves the Promises as an array issue and will display the actual data.
+    // console.log(movies);
+
+    return await renderMovies(movies);
+}
+
+function renderMovies(moviesArr) {
+    console.log(moviesArr);
+    const htmlOutput = moviesArr.map(movie => {
+        return `
+            <div class="movie-container">
+                <img class="movie-poster" src="${movie.Poster}" alt=""/>
+                <div class="movie-details">
+                    <h2 class="movie-title">${movie.Title}</h2>
+                    <div class="movie-rating">${movie.imdbRating}</div>
+                    <div class="movie-runtime">${movie.Runtime}</div>
+                    <div class="movie-genre">${movie.Genre}</div>
+                    <div class="add-movie-to-watchlist"> 
+                        <i></i>
+                        Watchlist
+                    </div>
+                    <div class="movie-plot">${movie.Plot}</div>
+                </div>
             </div>
-            <div class="movie-plot">${movie.Plot}</div>
-        </div>
-    </div>
-`
+        `
+        }).join('');
+    console.log(htmlOutput);;
 
-
+    return htmlOutput;
+}
